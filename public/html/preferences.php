ab788ec517a8a25f8e3cc738494fa20abf8be2e0
Sharif Tsang <114950879+ckxero@users.noreply.github.com>
2023-02-07 16:37:23 -0800

    added preference page

--- public/html/preferences.php
+++ public/html/preferences.php
@@ -7,69 +7,20 @@ require('../../app/init.php');
 <body>
     <div class="bg"></div>
     <?php include('../partials/header.php'); ?>
-    <main>
-        <section class="signup-form">
-            <div class="window">
-                <div class="window-header">
-                    <h3 class="window-title">Create Account</h3>
-                    <div class="window-icons">
-                        <img src="<?php echo get_public_url('/images/website-assets/win-icon1.png')?>" alt="" class="win-icon">
-                        <img src="<?php echo get_public_url('/images/website-assets/win-icon2.png')?>" alt="" class="win-icon">
-                        <img src="<?php echo get_public_url('/images/website-assets/win-icon3.png')?>" alt="" class="win-icon">
-                    </div>
-                </div>
-                <div class="window-content">
-                    <div class="container logoSpace">
-                        <h4 >WELCOME TO</h4>
-                        <img class="smlLogo" src="<?php echo get_public_url('/images/live2k_Logo_final.png')?>"  alt="Logo">
-                        <h4>YOUR JOURNEY AWAITS...</h4>
-                    </div>
-                    <div class="container">
-                        <form>
-                            <div class="flex">    
-                                <div class="flex row">
-                                    <div class="flex">
-                                        <label for="fname">First Name</label>
-                                        <input type="text" id="fname" name="fname">
-                                    </div>
-                                    <div class="flex">
-                                        <label for="lname">Last Name</label>
-                                        <input type="text" id="lname" name="lname">
-                                    </div>
-                                </div>
-                                <div class="flex">
-                                    <label for="email"><b>Email</b></label>
-                                    <input type="text" name="email" id="email" required>
-
-                                    <label for="psw"><b>Password</b></label>
-                                    <input type="password" name="psw" id="psw" required>
-
-                                    <label for="psw-repeat"><b>Repeat Password</b></label>
-                                    <input type="password" name="psw-repeat" id="psw-repeat" required>
-                                </div>
-                                <div class="flex">
-                                    <div class="check">
-                                        <input type="checkbox" class="cbox" id="terms" name="terms">
-                                        <label for="terms">I would like to receive news and upcoming promotions for LIVE2K</label>
-                                    </div>
-                                    <div class="check">
-                                        <input type="checkbox" class="cbox" id="terms" name="terms">
-                                        <label for="terms">I have read and agree to the <a class="lineLink" href="#">Terms & Privacy</a>.</label>
-                                    </div>
-                                </div>
-                                <div class="flex center">
-                                    <a href="preferences.php"><button type="submit" class="btn3">Create Account</button></a>
-                                </div>
-                            </div>
-                            
-                            <div class="signedIn">
-                                <p>Already have an account? <a class="lineLink" href="login.php">Sign in</a>.</p>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <main class="prefContainer">
+        <section class="prefBanner">
+            <div class="bannerMsg">
+                <h2>HELP US DEFINE YOUR EXPERIENCE</h2>
+                <h4>PERSONALIZE AND PLAN YOUR LIVE2K EXPERIENCE BY TELLING US YOUR FAVOURITE MUSIC GENRE.</h4>
+            </div>
+            <div class="genreBanner">
+                <img src="<?php echo get_public_url('/images/genre-banner-img.png')?>"  alt="genre_banner">
             </div>
         </section>
+        <section>
+            <p>preferences</p>
+        </section>
+
     </main>
     <?php include('../partials/footer.php'); ?>
 </body>
